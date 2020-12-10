3eb0f4e20637c5d678d194ffc1abe5c52289d028
Christoffer Fridlund <christoffer.fridlund@helsinki.fi>
2020-12-10 22:32:08 +0200

    Lifting the state of the ShirtPageList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import ShirtListPage from './components/ShirtListPage'
 
+
+const testShirts = [
+  {
+    color: [
+      "grey"
+    ],
+    id: "919865f33813410ce76c",
+    manufacturer: "nouke",
+    name: "WERHUNK LIGHT",
+    price: 47,
+    type: "shirts"
+  },
+  {
+    color: [
+      "grey"
+    ],
+    id: "919865f33813410ce761",
+    manufacturer: "nouke",
+    name: "WERHUNK LIGHT",
+    price: 47,
+    type: "shirts"
+  },
+  {
+    color: [
+      "grey"
+    ],
+    id: "919865f33813410ce762",
+    manufacturer: "nouke",
+    name: "WERHUNK LIGHT",
+    price: 47,
+    type: "shirts"
+  }
+];
+
+
 const App = (props) => {
+  const [shirts, setShirts] = useState([])
+
+  useEffect(() => {
+    console.log('Fetching shirts');
+    axios
+      .get('https://bad-api-assignment.reaktor.com/products/shirts')
+      .then(response => {
+        console.log('get promise fulfilled');
+        console.log(response);
+        setShirts(response.data);
+        // setShirts(testShirts);
+      }).catch(error => {
+        console.log(error);
+      })
+
+    axios
+      .get('https://bad-api-assignment.reaktor.com/availability/derp')
+      .then(response => {
+        console.log('get promise fulfilled');
+        console.log(response);
+        // setShirts(response.data);
+        // setShirts(testShirts);
+      }).catch(error => {
+        console.log(error);
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <div>
-        <ShirtListPage />
+        <ShirtListPage shirts={shirts} />
       </div>
     </div>
   );
--- src/components/ShirtListPage.js
+++ src/components/ShirtListPage.js
@@ -1,62 +1,11 @@
-import React, { useState, useEffects, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 import ShirtPage from './ShirtPage';
 
 
-const testShirts = [
-  {
-    color: [
-      "grey"
-    ],
-    id: "919865f33813410ce76c",
-    manufacturer: "nouke",
-    name: "WERHUNK LIGHT",
-    price: 47,
-    type: "shirts"
-  },
-  {
-    color: [
-      "grey"
-    ],
-    id: "919865f33813410ce761",
-    manufacturer: "nouke",
-    name: "WERHUNK LIGHT",
-    price: 47,
-    type: "shirts"
-  },
-  {
-    color: [
-      "grey"
-    ],
-    id: "919865f33813410ce762",
-    manufacturer: "nouke",
-    name: "WERHUNK LIGHT",
-    price: 47,
-    type: "shirts"
-  }
-];
-
-
-const ShirtListPage = (props) => {
-  const [shirts, setShirts] = useState([])
-
-  useEffect(() => {
-    console.log('Fetching shirts');
-    axios
-      .get('https://bad-api-assignment.reaktor.com/products/shirts')
-      .then(response => {
-        console.log('get promise fulfilled');
-        console.log(response);
-        setShirts(response.data);
-        // setShirts(testShirts);
-      }).catch(error => {
-        console.log(error);
-      })
-  }, [])
+const ShirtListPage = ({ shirts }) => {
 
   const shirtList = shirts.map(shirt => {
-    // console.log(shirt.id)
     return (
       <li
         key={shirt.id}
